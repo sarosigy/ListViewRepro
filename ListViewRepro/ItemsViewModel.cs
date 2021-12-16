31b8c59e9bde4577495e54653d5714e871f1ebbd
sarosigy <sarosigy@DEV2022>
2021-12-16 14:25:02 +0100

    Add project files.

---
+++ ListViewRepro/ItemsViewModel.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace ListViewRepro
+{
+    internal class ItemsViewModel
+    {
+        public ObservableCollection<GroupInfoList> Available { get; set; }
+        public ObservableCollection<Contact> Selected { get; set; }
+
+        private ItemsViewModel() { }
+
+        public static async Task<ItemsViewModel> CreateAsync()
+        {
+            ItemsViewModel itemsViewModel = new ItemsViewModel();
+            itemsViewModel.Available = await GetContactsGroupedAsync();
+            itemsViewModel.Selected = new ObservableCollection<Contact>();
+
+            return itemsViewModel;
+        }
+
+        // C# Code
+
+        // To create a collection of grouped items, create a query that groups
+        // an existing list, or returns a grouped collection from a database.
+        // The following method is used to create the ItemsSource for our CollectionViewSource:
+
+        public static async Task<ObservableCollection<GroupInfoList>> GetContactsGroupedAsync()
+        {
+            // Grab Contact objects from pre-existing list (list is returned from function GetContactsAsync())
+            var query = from item in await GetContactsAsync()
+
+                            // Group the items returned from the query, sort and select the ones you want to keep
+                        group item by item.LastName.Substring(0, 1).ToUpper() into g
+                        orderby g.Key
+
+                        // GroupInfoList is a simple custom class that has an IEnumerable type attribute, and
+                        // a key attribute. The IGrouping-typed variable g now holds the Contact objects,
+                        // and these objects will be used to create a new GroupInfoList object.
+                        select new GroupInfoList(g) { Key = g.Key };
+            
+            return new ObservableCollection<GroupInfoList>(query);
+        }
+
+        public static async Task<ObservableCollection<Contact>> GetContactsAsync()
+        {
+            StorageFile file = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///Assets/Contacts.txt", UriKind.Absolute)).AsTask().ConfigureAwait(false);
+            IList<string> lines = await FileIO.ReadLinesAsync(file);
+
+            ObservableCollection<Contact> contacts = new ObservableCollection<Contact>();
+
+            for (int i = 0; i < lines.Count; i += 3)
+            {
+                contacts.Add(new Contact(lines[i], lines[i + 1], lines[i + 2]));
+            }
+            return contacts;
+        }
+
+        private (GroupInfoList, Contact) FindInAvailable(string name)
+        {
+            foreach(var gitem in Available)
+                foreach(var item in gitem)
+                    if (item is Contact c && c.Name == name)
+                        return (gitem, c);
+            return (null, null);
+        }
+
+        internal void MoveToSelected(string moved)
+        {
+            (GroupInfoList groupInfo, Contact contact) = FindInAvailable(moved);
+            if (groupInfo != null)
+            {
+                groupInfo.Remove(contact);
+                if (groupInfo.Count == 0)
+                    Available.Remove(groupInfo);
+                Selected.Add(contact);
+            }
+        }
+
+        internal void MoveToAvailable(string moved)
+        {
+            Contact contact = Selected.FirstOrDefault(i => i.Name == moved);
+            string key = contact.LastName.Substring(0, 1).ToUpper();
+            Selected.Remove(contact);
+            
+            GroupInfoList list = Available.FirstOrDefault(i => (string)i.Key == key);
+            if (list == null)
+            {
+                list = new GroupInfoList(new[] { contact }) { Key = key };
+                Available.Add(list);
+            }
+            else
+            {
+                list.Add(contact);
+            }
+        }
+    }
+
+    // GroupInfoList class definition:
+    public class GroupInfoList : ObservableCollection<object>
+    {
+        public GroupInfoList(IEnumerable<object> items) : base(items)
+        {
+        }
+        public object Key { get; set; }
+    }
+
+    // Contact class definition:
+    public class Contact
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Company { get; private set; }
+        public string Name => FirstName + " " + LastName;
+
+        public Contact(string firstName, string lastName, string company)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Company = company;
+        }
+
+        // ... Methods ...
+    }
+}
---
+++ ListViewRepro/MainPage.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ListViewRepro
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ItemsViewModel _itemsViewModel;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            _itemsViewModel = await ItemsViewModel.CreateAsync();
+            ContactsCVS.Source = _itemsViewModel.Available;
+            lvSelectedItems.ItemsSource = _itemsViewModel.Selected;
+        }
+
+        private void lvSelectedItems_DragItemsStarting(object sender, DragItemsStartingEventArgs e)
+        {
+            if (e.Items.First() is Contact contact)
+            {
+                e.Data.SetText(contact.Name);
+                e.Data.RequestedOperation = DataPackageOperation.Move;
+            }
+        }
+
+        private void lvSelectedItems_DragOver(object sender, DragEventArgs e)
+        {
+            e.AcceptedOperation = (e.DataView.AvailableFormats.Contains(StandardDataFormats.Text)) ? DataPackageOperation.Move : DataPackageOperation.None;
+        }
+
+        private async void lvSelectedItems_Drop(object sender, DragEventArgs e)
+        {
+            var def = e.GetDeferral();
+            string moved = await e.DataView.GetTextAsync();
+            _itemsViewModel.MoveToSelected(moved);
+            def.Complete();
+        }
+
+        private void lvAvailableItems_DragItemsStarting(object sender, DragItemsStartingEventArgs e)
+        {
+            if (e.Items.First() is Contact contact)
+            {
+                e.Data.SetText(contact.Name);
+                e.Data.RequestedOperation = DataPackageOperation.Move;
+            }
+        }
+
+        private void lvAvailableItems_DragOver(object sender, DragEventArgs e)
+        {
+            e.AcceptedOperation = (e.DataView.AvailableFormats.Contains(StandardDataFormats.Text)) ? DataPackageOperation.Move : DataPackageOperation.None;
+        }
+
+        private async void lvAvailableItems_Drop(object sender, DragEventArgs e)
+        {
+            var def = e.GetDeferral();
+            string moved = await e.DataView.GetTextAsync();
+            _itemsViewModel.MoveToAvailable(moved);
+            def.Complete();
+        }
+    }
+}
